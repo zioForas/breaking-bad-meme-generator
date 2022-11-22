f5e51cc880da3ff36b309da32c5aadd647d37f61
Anto <107312386+zioForas@users.noreply.github.com>
2022-11-22 12:45:16 +0000

    Breaking Bad Meme Generator

---
+++ br-meme/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://i.ytimg.com/vi/ohKfrH3Xm-A/hqdefault.jpg" 
+                className="header--image"
+            />
+            <h2 className="header--title">Breaking Bad MEME Generator</h2>
+        </header>
+    )
+}
